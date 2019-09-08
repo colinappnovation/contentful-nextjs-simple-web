665fc6a22caded6428f8e5f4f4e7bf1e5b143d94
Colin McClure <colin@appnovation.com>
2019-09-08 20:09:45 +0100

    feat: Rework the blogs listing component

--- components/blogs.js
+++ components/blogs.js
@@ -1,46 +1,49 @@
-import client from '../lib/client';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import api from '../lib/api'
 
 /*
     Use effect means no SSR therefore no content on view source etc.
  */
 
 const BlogListing = (props) => {
-	const [ blogListing, setBlogListing ] = useState([]);
-    const { filter } = props;
-	async function getEntries(filter) {
-        console.log('filter which came in:', filter)
-		const entries = await client.getEntries({
-			content_type: 'post',
-			'sys.id[ne]': filter
-		});
-		const { items } = entries;
 
-		const listing = items.map((b) => {
-			const { title, slug } = b.fields;
-			return { title, slug };
-		});
+	const { blogs } = props
 
-		return listing;
-	}
+	// const [ blogListing, setBlogListing ] = useState([]);
+    // const { filter } = props;
+	// async function getEntries(filter) {
+    //     console.log('filter which came in:', filter)
+	// 	const entries = await client.getEntries({
+	// 		content_type: 'post',
+	// 		'sys.id[ne]': filter
+	// 	});
+	// 	const { items } = entries;
 
-	useEffect(() => {		
-		async function getBlogs() {
-			const blogs = await getEntries(filter);
-			console.log('fetching blogs for sidebar...', blogs);
-			setBlogListing(blogs);
-		}
-		getBlogs();
-	}, [filter]);
+	// 	const listing = items.map((b) => {
+	// 		const { title, slug } = b.fields;
+	// 		return { title, slug };
+	// 	});
+
+	// 	return listing;
+	// }
+
+	// useEffect(() => {		
+	// 	async function getBlogs() {
+	// 		const blogs = await getEntries(filter);
+	// 		console.log('fetching blogs for sidebar...', blogs);
+	// 		setBlogListing(blogs);
+	// 	}
+	// 	getBlogs();
+	// }, [filter]);
 
 	return (
 		<div className="float-right bg-gray-200 p-4 m-5">
-			{console.log('In the component..', blogListing)}
+			{console.log('In the component..', blogs)}
 			<h2 className="uppercase mb-5 ">More from the blog</h2>
 			<ul className="list-none sm:list-disc md:list-decimal lg:list-disc xl:list-none">
-				{blogListing.length > 0 ? (
-					blogListing.map((b) => {
+				{blogs.length > 0 ? (
+					blogs.map((b) => {
 						return (
 							<li>
 								<Link href="/posts/[id]" as={`${b.slug}`}>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,15 +5,16 @@ import ErrorPage from "next/error";
 import BlogListing from "../../components/blogs";
 import api from "../../lib/api";
 
-const Page = blog => {
+const Page = props => {
  
   // Loading a page which has been unpublished!
-  if (blog.fields == undefined) {
+  if (props.blog.fields == undefined) {
     return <ErrorPage statusCode="404" />;
   }
   
+  const { blog, blogs } = props
+
   const { title, date, body, hero } = blog.fields;
-  const { id: ident } = blog.sys;
   const src = (hero) ? hero.fields.file.url : "";
   const fdate = day(date).format("DD MMMM YYYY");
 
@@ -28,16 +29,24 @@ const Page = blog => {
         <strong>Published:</strong> {fdate}
       </p>
       <img src={`${src}?w=1440&h=600`} />
-      <BlogListing filter={ident} />
+      <BlogListing blogs={blogs} />
       <div id="body" className="mt-8" dangerouslySetInnerHTML={getMarkup()} />
     </Layout>
   );
 };
 
-Page.getInitialProps = async context => {
-  const { id } = context.query;
+Page.getInitialProps = async ({query}) => {
+
+  const { id } = query;
   const { items, total } = await api.getPostBySlug('post', id);
-  return total == 1 ? items[0] : {}
+
+  if (total == 1) {
+    const blog = items[0]
+    const blogs = await api.getBlogsWithNotInclude(blog.sys.id)    
+    return {blog, blogs}
+  } 
+  
+  return {} 
 }
 
 export default Page;
