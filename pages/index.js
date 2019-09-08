6d4be1ecc79e1af015afcaf2f2cd8b0b088fbb16
Colin McClure <colin@appnovation.com>
2019-09-08 16:45:27 +0100

    feat: Implement API access for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Post from '../components/post';
 import Layout from '../components/layout';
-import client from '../lib/client';
+import api from '../lib/api'
 // import { useEffect, useState } from 'react';
 
 /*
@@ -60,28 +60,8 @@ const HomePage = (props) => {
 // Intentionally left here for example.
 
 HomePage.getInitialProps = async () => {
-   async function fetchContentTypes() {
-    const types = await client.getContentTypes()
-    if (types.items) return types.items
-    console.log('Error getting Content Types.')
-  }
-  async function fetchEntriesForContentType(contentType) {
-    const entries = await client.getEntries({
-      content_type: contentType.sys.id
-    })
-    if (entries.items) return entries.items
-    console.log(`Error getting Entries for ${contentType.name}.`)
-  }
-
-  async function getPosts() {
-    const contentTypes = await fetchContentTypes()
-    const allPosts = await fetchEntriesForContentType(contentTypes[0])
-    console.log('Fetching records for homepage')
-    return allPosts
-  }
-
    return {
-     posts: [...await getPosts()]
+     posts: await api.getPosts()
    }
 }
 
