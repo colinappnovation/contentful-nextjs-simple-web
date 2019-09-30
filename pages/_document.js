877924f2da154d6399c938193732e6afc5fb7dcd
Colin McClure <colin@appnovation.com>
2019-09-30 19:42:59 +0100

    feat: Addition of favicon and assorted link statements

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png"></link>
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"></link>
+          <link rel="manifest" href="/static/site.webmanifest"></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
