687017940785aa5855a7238d52494343bfac8575
Colin McClure <colin@appnovation.com>
2019-09-30 16:06:46 +0100

    feat: Addition of webp support listing

--- components/post.js
+++ components/post.js
@@ -9,7 +9,10 @@ function Post({ id, title, slug, hero, standfirst, date, author}) {
 
     return (
       <div className="max-w-sm rounded overflow-hidden shadow-lg m-1">
-        <img className="w-full" src={`${hero.fields.file.url}?w=900&fit=scale&h=450`}></img>
+        <picture>
+          <source srcset={`${hero.fields.file.url}?fm=webp&w=900&fit=scale&h=450`} type="image/webp"></source>
+          <img className="w-full" src={`${hero.fields.file.url}?w=900&fit=scale&h=450`}></img>
+        </picture>        
         <div className="px-6 py-4" key={id}>
           <div className="font-bold text-xl mb-2">           
               <Link href="/posts/[id]" as={`posts/${slug}`} >
@@ -19,7 +22,10 @@ function Post({ id, title, slug, hero, standfirst, date, author}) {
               <p className="text-gray-700 text-base">{standfirst}</p>
           </div>
           <div className="flex items-center">
-            <img className="w-10 h-10 rounded-full mr-4" src={`${url}`} alt={`Avatar of ${fullname}`} />
+            <picture>
+              <source srcset={`${url}?fm=webp`} type="image/webp"></source>
+              <img className="w-10 h-10 rounded-full mr-4" src={`${url}`} alt={`Avatar of ${fullname}`} />
+            </picture>            
             <div className="text-sm">
               <p className="text-gray-500 text-sm">{fullname}</p>
               <p className="text-gray-500 text-sm mt-0">{jobTitle}</p>
