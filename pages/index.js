b89d0435c8e6c190eb5a49c358c8f1c4b54a00e4
Colin McClure <colin@appnovation.com>
2019-10-06 15:30:33 +0100

    feat: Tidy and bring in microcopy concept on homepage

--- pages/index.js
+++ pages/index.js
@@ -49,16 +49,12 @@ import api from '../lib/api'
 
 	return (
 		<Layout>
-			<h1 className="uppercase text-4xl text-gray-800">Insights</h1>
+			<h1 className="uppercase text-4xl text-gray-800">{props.microcopy[1].fields.value}</h1>
 			<p className="text-xl mb-5 text-gray-600">
-				To understand the future you have to be grounded in the now. We guide our clients as their industries
-				evolve with the changing digital landscape.
+				{props.microcopy[0].fields.value}
 			</p>
 			<div className="flex flex-wrap mb-4">
-				{/* <PostList /> */}
-
-				{props.posts.length > 0 ? (
-					props.posts.map((p) => (
+				{props.posts.map((p) => (
 						<Post
 							title={p.fields.title}
 							date={p.fields.date}
@@ -69,19 +65,19 @@ import api from '../lib/api'
 							key={p.sys.id}
 						/>
 					))
-				) : null}	
-
+				}
 			</div>
 		</Layout>
-	);
-};
+	)
+}
 
 // STANDARD APPROACH using getInitialProps
 // Intentionally left here for example.
 
 HomePage.getInitialProps = async () => {
    return {
-     posts: await api.getPosts()
+	 posts: await api.getPosts(),
+	 microcopy: await api.getCopy()
    }
 }
 
